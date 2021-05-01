da107b27575ae236655a70f3c59bc3701b304ab5
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2021-05-01 22:15:52 +0200

    feat(merge_request): return get merge request

--- src/gitlab_client.rs
+++ src/gitlab_client.rs
@@ -1,29 +1,29 @@
 use reqwest;
+use serde::Deserialize;
 use crate::coffee_config::Gitlab;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct MergeRequest {
     pub title: String,
-    pub author: String,
-    pub date: String,
+    pub author: Author,
+    pub created_at: String,
     pub upvotes: i8,
-    pub url: String,
+    pub web_url: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Author {
+    pub name: String,
 }
 
 impl MergeRequest {
     pub async fn get_open(config: &Gitlab) -> Result<Self, Box<dyn std::error::Error>> {
         let response = reqwest::get(&config.base_url)
+            .await?
+            .json::<MergeRequest>()
             .await?;
-        println!("Settings {:?}", config);
-        println!("Response {:?}", response);
-
-        Ok(MergeRequest {
-            title: "self".to_string(),
-            author: "test".to_string(),
-            date: "today".to_string(),
-            upvotes: 2,
-            url: "https://gitlab.com/epqp".to_string(),
-        })
+        
+        Ok(response)
     }
 }
 
@@ -36,16 +36,34 @@ mod tests {
     #[tokio::test]
     async fn it_works() {
         let server = MockServer::start();
+        let data = r#"{
+            "title": "Whatever",
+            "author": {
+                "name": "Author Name"
+            },
+            "created_at": "2021-05-01T00:00:00Z",
+            "upvotes": 1,
+            "web_url": "https://test.gitlab.com/projects/x/mrs/1"
+        }"#;
+        
         let mock = server.mock(|when, then| {
             when.method(GET);
             then.status(200)
-                .header("Content-Type", "text/html; charset=UTF-8")
-                .body("pong");
+                .header("Content-Type", "application/json")
+                .body(data);
         });
         let config = Gitlab::new(server.base_url().to_string(), "TOKEN".to_string());
         let response = MergeRequest::get_open(&config).await;
-        println!("Hello {:?}", response);
+        // Exactly one HTTP method that matched the mock requirements
         mock.assert();
-        assert_eq!(2, 2);
+        assert_eq!(response.is_ok(), true);
+        let result = response.unwrap();
+        assert_eq!(result, MergeRequest {
+            title: "Whatever".to_string(),
+            author: Author { name: "Author Name".to_string() },
+            created_at: "2021-05-01T00:00:00Z".to_string(),
+            upvotes: 1,
+            web_url: "https://test.gitlab.com/projects/x/mrs/1".to_string(),
+        });
     }
 }
